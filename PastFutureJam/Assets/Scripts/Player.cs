44af7b94af5e60eeac0f6296566b588d2e38b9b7
Eric Polman <ericpolman900@gmail.com>
2014-11-22 14:38:24 +0100

    Separated Future and Past movement

--- PastFutureJam/Assets/Scripts/Player.cs
+++ PastFutureJam/Assets/Scripts/Player.cs
@@ -2,70 +2,83 @@
 using System.Collections;
 
 
-public class Player : MonoBehaviour {
-	
-    private Vector2 _startPos;
-    private Vector2 _endPos;
-    private bool moving = false;
-    private float time = 0f;
-    public float walkTime = 0.5f;
+public class Player : MonoBehaviour
+{
 
-	
-	void Start () {
+  private Vector2 _startPos;
+  private Vector2 _endPos;
+  private bool moving = false;
+  private float time = 0f;
+  public float walkTime = 0.5f;
 
-        transform.position = new Vector2(Mathf.Ceil(transform.position.x), Mathf.Ceil(transform.position.y));
-	}
-	
-	void Update () {
-		
-    	if(moving) {
-            time += Time.deltaTime;
+  public bool fromTheFuture = false;
+  private WorldSwitcher _worldSwitcher;
 
-			transform.position = Vector2.Lerp(_startPos, _endPos, time/walkTime);
-            if (time >= walkTime)
-            {
-                moving = false;
-                time = 0;
-            }
-        }
-        else
-        {
-            CheckInput();
-        }
-	}
-	
-	private void CheckInput() {
-		float moveX = Input.GetAxisRaw ("Horizontal");
-		if (moveX != 0){
-            _startPos = transform.position;
-			_endPos = _startPos + Vector2.right * moveX;
-			moving = true;
-            time = 0;
-		}
+  void Start()
+  {
+    transform.position = new Vector2(Mathf.Ceil(transform.position.x), Mathf.Ceil(transform.position.y));
+    _worldSwitcher = Camera.main.GetComponent<WorldSwitcher>();
+  }
 
-		float moveY = Input.GetAxisRaw ("Vertical");
-		
-		if (moveY != 0){
-            _startPos = transform.position;
-            _endPos = _startPos + Vector2.up * moveY;
-			moving = true;
-            time = 0;
-		}
+  void Update()
+  {
 
-        if (collides(_endPos))
-            moving = false;
-
-	}
-
-    private bool collides(Vector2 tile)
+    if (moving)
     {
-        Vector2[] tileList = new Vector2[] { new Vector2(-1, 0), new Vector2(-1, 1), new Vector2(-1, -1) };
-        foreach (Vector2 element in tileList)
-        {
-            if (tile == element)
-                return true;
-            
-        }
-        return false;
+      time += Time.deltaTime;
+
+      transform.position = Vector2.Lerp(_startPos, _endPos, time / walkTime);
+      if (time >= walkTime)
+      {
+        moving = false;
+        time = 0;
+      }
     }
+    else
+    {
+      if(fromTheFuture && _worldSwitcher.currentWorld == World.FUTURE ||
+        !fromTheFuture && _worldSwitcher.currentWorld == World.PAST)
+      {
+        CheckInput();
+      }
+    }
+  }
+
+  private void CheckInput()
+  {
+    float moveX = Input.GetAxisRaw("Horizontal");
+    if (moveX != 0)
+    {
+      _startPos = transform.position;
+      _endPos = _startPos + Vector2.right * moveX;
+      moving = true;
+      time = 0;
+    }
+
+    float moveY = Input.GetAxisRaw("Vertical");
+
+    if (moveY != 0)
+    {
+      _startPos = transform.position;
+      _endPos = _startPos + Vector2.up * moveY;
+      moving = true;
+      time = 0;
+    }
+
+    if (collides(_endPos))
+      moving = false;
+
+  }
+
+  private bool collides(Vector2 tile)
+  {
+    Vector2[] tileList = new Vector2[] { new Vector2(-1, 0), new Vector2(-1, 1), new Vector2(-1, -1) };
+    foreach (Vector2 element in tileList)
+    {
+      if (tile == element)
+        return true;
+
+    }
+    return false;
+  }
 }
