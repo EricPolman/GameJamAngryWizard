d27f8b81f630030b45d237acafff1d6125d1d805
Eric Polman <ericpolman900@gmail.com>
2014-11-22 10:57:07 +0100

    Added basic world switching (with left CTRL)

---
+++ PastFutureJam/Assets/Scripts/WorldSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum World { PAST, FUTURE };
+
+public class WorldSwitcher : MonoBehaviour
+{
+  public Transform futureStartPoint;
+  public Transform pastStartPoint;
+
+  public World currentWorld;
+
+	void Start ()
+	{
+    transform.position = 
+      currentWorld == World.PAST ? pastStartPoint.position : futureStartPoint.position;
+    transform.position -= Vector3.forward * 10;
+	}
+	
+	void Update ()
+	{
+	  if(Input.GetKeyDown(KeyCode.LeftControl))
+    {
+      SwitchWorld();
+    }
+	}
+
+  void SwitchWorld()
+  {
+    currentWorld = currentWorld == World.PAST ? World.FUTURE : World.PAST;
+
+    transform.position =
+      currentWorld == World.PAST ? pastStartPoint.position : futureStartPoint.position;
+    transform.position -= Vector3.forward * 10;
+  }
+}
